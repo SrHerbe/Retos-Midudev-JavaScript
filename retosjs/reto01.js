bd7c8f89a837857f4d9aede273e8332200e0b0f6
SrHerbe <60781159+SrHerbe@users.noreply.github.com>
2022-12-05 18:37:18 +0100

    Update reto01.js

--- retosjs/reto01.js
+++ retosjs/reto01.js
@@ -1,3 +1,18 @@
+/* 
+Crea un programa que envuelva con "*" los regalos
+
+const gifts = ['cat', 'game', 'socks']
+const wrapped = wrapping(gifts)
+
+console.log(wrapped)
+[
+  "*****\\n*cat*\\n*****",
+  "******\\n*game*\\n******",
+  "*******\\n*socks*\\n*******"
+] 
+
+*/
+
 function wrapping(gifts) {
     //Map reitera todo el array
     return gifts.map((item) => {
