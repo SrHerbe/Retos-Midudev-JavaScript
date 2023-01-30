16adf2b3c23ed13fab226c610c0db8807be87c9e
SrHerbe <60781159+SrHerbe@users.noreply.github.com>
2023-01-30 10:25:17 +0100

    Update reto10.js

--- retosjs/reto10.js
+++ retosjs/reto10.js
@@ -1,1 +1,8 @@
-
+function checkJump(heights) {
+  return heights.reduce((a,e,i,arr) =>{
+    const dif = e-arr[i-1],      
+          direction = +(dif>0);          
+    if ( i>0 && dif!==0 && a.at(-1)!=direction ) a.push(direction);
+    return a;
+    },[]).join("") === "10";
+}
