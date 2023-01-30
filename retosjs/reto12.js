a4263d1cbd93657a504dafeeb4d19dff43321421
SrHerbe <60781159+SrHerbe@users.noreply.github.com>
2023-01-30 10:25:56 +0100

    Update reto12.js

--- retosjs/reto12.js
+++ retosjs/reto12.js
@@ -1,1 +1,18 @@
-
+function selectSleigh(distance, sleighs) {
+  
+  let mejor = null;
+  
+  sleighs.forEach((v, i) => {
+      sleighs[i].consumption = v.consumption*distance;
+  });
+  
+  sleighs.reverse().every((v) => {
+      if (v.consumption > 20) {
+          return true;
+      } else {
+          mejor = v.name;
+      }
+  });
+  
+  return mejor;
+}
