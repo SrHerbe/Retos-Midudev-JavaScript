0bf9102068cf747454c85d6b344cff0ead1a724c
SrHerbe <60781159+SrHerbe@users.noreply.github.com>
2023-01-30 10:24:42 +0100

    Update reto05.js

--- retosjs/reto05.js
+++ retosjs/reto05.js
@@ -1,1 +1,13 @@
+function getMaxGifts(giftsCities, maxGifts, maxCities) {
+	const g = giftsCities.sort((a, b) => {
+			if (a > b) return -1;
+			return 0;
+		}).filter((element) => element < maxGifts);
 
+
+	g.length = maxCities;
+	const result = g.reduce((a, b) => a + b, 0);
+	if (result > maxGifts) return maxGifts;
+
+	return result;
+}
